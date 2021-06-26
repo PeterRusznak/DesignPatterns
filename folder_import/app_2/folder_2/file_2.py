17cfbcbfb07baa231625b5124b38334da6bd5ba9
PeterRusznak <pet.rusznak@gmail.com>
2021-06-26 12:57:30 +0200

    better import

--- folder_import/app_2/folder_2/file_2.py
+++ folder_import/app_2/folder_2/file_2.py
@@ -1,9 +1,11 @@
-import os,sys
-# os.path.join(os.path.curdir, 'file.name')
-sys.path.append("/home/pr/gyak/DesignPatterns/folder_import/app_1/folder_1")
-# print(sys.path)
+import sys
+from os.path import abspath
+
+elso_dir = abspath( "app_1"+ '/folder_1/')
+sys.path.append(elso_dir)
 
 from file_1 import First
+
 
 f = First()
 f.first()
